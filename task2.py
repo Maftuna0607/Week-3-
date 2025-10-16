37a62f749e62403c963a93872a238c37ae9c48fa
Maftuna <maftunarouzmetova@gmail.com>
2025-10-16 12:55:43 +0500

    added Fibonacci sequence program

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 print("--- Running Total Calculator ---")
 total=0
 while True:
-    user_input=input("Enter numbers to add them up or type 'done' to see the total result. ")
+    user_input=input("Enter numbers to add them up or type 'done' to see the total result: ")
     if user_input == "done":
         break
     total+= float(user_input)
